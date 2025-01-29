00952c582675e1519a537f38f93a8953a04ade0a
PVP1010 <Valera10102791@mail.ru>
2025-01-29 16:46:25 +0700

    add index.html

--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -3,14 +3,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # 3 на главной странице будет выводится данный текст
+
 def index(request):
-    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+    return render(request, 'main/index.html')
 
 def new(request):
     return HttpResponse("<h1>Это вторая страница мого проекта на Django</h1>")
-
-def data(request):
-    return HttpResponse("<h1>Домашнее задание. Страница data мого проекта на Django</h1>")
-
-def test(request):
-    return HttpResponse("<h1>Домашнее задание. Страница test мого проекта на Django</h1>")
