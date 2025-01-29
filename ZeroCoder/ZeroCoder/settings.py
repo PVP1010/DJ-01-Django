ef90fdd1de3e1dd70840eeeacfe8d42828992f98
PVP1010 <Valera10102791@mail.ru>
2025-01-29 23:42:13 +0700

    add

--- ZeroCoder/ZeroCoder/settings.py
+++ ZeroCoder/ZeroCoder/settings.py
@@ -106,9 +106,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/'                                           # путь для статических файлов!!!!!!!!!!!!!!!!
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = [                                           # путь для статических файлов
+    BASE_DIR / "main" / "static"
+]
